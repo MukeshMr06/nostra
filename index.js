bce18c4f19a01d15357cc63389c0c3185f1e726f
MukeshMr06 <mukeshmr0602@gmail.com>
2025-06-02 19:48:51 -0500

    updated all files

---
+++ index.js
@@ -0,0 +1,19 @@
+var navebar = document.getElementById("navebar")
+var closenav = document.getElementById("close")
+
+closenav.addEventListener("click",function(){
+    navebar.style.marginTop="-25px"
+})
+
+// header section
+
+var sidenav= document.getElementById("sidenav")
+var menu = document.getElementById("menu")
+var closenav = document.getElementById("closenav")
+
+menu.addEventListener("click",function(){
+    sidenav.style.left=0 
+})
+closenav.addEventListener("click",function(){
+    sidenav.style.left="-40%"
+})
